b8039ca284896fcedf61705fa3ebf1e459fac595
R3MODAS <remodas7774@gmail.com>
2023-02-11 19:44:15 +0530

    commit

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -9,8 +9,7 @@ export default function Alert(props) {
     <div style={{height: "50px"}}>
       {props.alert && <div
         className={`alert alert-${props.alert.type} alert-dismissible fade show`}
-        role="alert"
-      >
+        role="alert">
         <strong>{capitalize(props.alert.type)}!!</strong> : {props.alert.msg}
       </div>}
     </div>
